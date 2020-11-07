9d97da6dcf7226e373cc6dd5393c38376db45691
Abbie <abbieadamsthompson@gmail.com>
2020-11-07 17:04:14 -0600

    Add poll functionality
    Copied poll functionality from other bot
    Todo: Comment, More functionalities? (Perhaps results?)

--- pollcommands.py
+++ pollcommands.py
@@ -1,11 +1,41 @@
 import discord
 from discord.ext import commands, tasks
+import emoji
+import string
 
 class PollCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    @commands.command(aliases=["Poll"])
+    async def poll(self, ctx, *args):
+        numoptions = len(args)-1
+        usethumbs = False
+        temp = list(args)
+        description = ""
+        reactions = []
+        question = str(args[0])
+        temp.pop(0)
+        if numoptions <= 0:
+            await ctx.send("I need options to list.")
+            return
+        if numoptions == 1:
+            await ctx.send("We can't have a poll with one option, dummy.")
+            return
+        if numoptions == 2:
+            usethumbs = True
+        if usethumbs == True:
+            description = ":thumbsup: {0}\n\n\n:thumbsdown: {1}".format(str(args[1]),str(args[2]))
+            reactions = [":thumbsup:",":thumbsdown:"]
+        else:
+            for count,arg in enumerate(temp):
+                reactions.append(":regional_indicator_symbol_letter_{0}:".format(string.ascii_lowercase[count]))
+                description += ":regional_indicator_{1}: {0}\n\n".format(arg,string.ascii_lowercase[count])
+        embed = discord.Embed(description=description)
+        msg = await ctx.send("**{0}**".format(question,embed=embed)
+        for emote in reactions:
+            await msg.add_reaction(emoji.emojize(emote,use_aliases=True))
+        return
 
 def setup(bot):
     bot.add_cog(PollCommands(bot))
